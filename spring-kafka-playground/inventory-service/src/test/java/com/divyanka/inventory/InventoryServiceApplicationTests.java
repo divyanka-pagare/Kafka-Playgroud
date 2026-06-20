c970359980094265874e5586923266e874e5a4e1
dell <pagaredivyanka@gmail.com>
2026-06-20 11:16:04 +0530

    initializing 3 services order-service, notification-service, payment-service

---
+++ spring-kafka-playground/inventory-service/src/test/java/com/divyanka/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyanka.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
